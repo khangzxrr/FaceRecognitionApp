1ed415b662aa9f5b244d55b657e7816900ae3b54
khang <khangzxrr@gmail.com>
2020-10-05 22:52:27 +0700

    Add project files.

---
+++ FaceRecognitionApp/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace FaceRecognitionApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.kythiField = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.khoangayField = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.monthiField = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tongsophongField = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.nhapdanhsachBtn = new System.Windows.Forms.Button();
+            this.nextBtn = new System.Windows.Forms.Button();
+            this.sangRadio = new System.Windows.Forms.RadioButton();
+            this.chieuRadio = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.tongsophongField)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kythiField
+            // 
+            this.kythiField.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kythiField.Location = new System.Drawing.Point(221, 83);
+            this.kythiField.Name = "kythiField";
+            this.kythiField.Size = new System.Drawing.Size(411, 34);
+            this.kythiField.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(134, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kỳ Thi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(147, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(553, 58);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ỨNG DỤNG\r\nNHẬN DẠNG THÍ SINH HỖ TRỢ CÁN BỘ COI THI";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(84, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Khóa Ngày";
+            // 
+            // khoangayField
+            // 
+            this.khoangayField.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.khoangayField.Location = new System.Drawing.Point(221, 141);
+            this.khoangayField.Name = "khoangayField";
+            this.khoangayField.Size = new System.Drawing.Size(411, 34);
+            this.khoangayField.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(114, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 29);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Môn Thi";
+            // 
+            // monthiField
+            // 
+            this.monthiField.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.monthiField.Location = new System.Drawing.Point(221, 202);
+            this.monthiField.Name = "monthiField";
+            this.monthiField.Size = new System.Drawing.Size(411, 34);
+            this.monthiField.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(114, 257);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 29);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Buổi Thi";
+            // 
+            // tongsophongField
+            // 
+            this.tongsophongField.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tongsophongField.Location = new System.Drawing.Point(236, 300);
+            this.tongsophongField.Name = "tongsophongField";
+            this.tongsophongField.Size = new System.Drawing.Size(154, 34);
+            this.tongsophongField.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(10, 302);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(207, 29);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Tổng số phòng thi";
+            // 
+            // nhapdanhsachBtn
+            // 
+            this.nhapdanhsachBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nhapdanhsachBtn.Location = new System.Drawing.Point(221, 354);
+            this.nhapdanhsachBtn.Name = "nhapdanhsachBtn";
+            this.nhapdanhsachBtn.Size = new System.Drawing.Size(411, 43);
+            this.nhapdanhsachBtn.TabIndex = 13;
+            this.nhapdanhsachBtn.Text = "Nhập danh sách thí sinh";
+            this.nhapdanhsachBtn.UseVisualStyleBackColor = true;
+            this.nhapdanhsachBtn.Click += new System.EventHandler(this.nhapdanhsachBtn_Click);
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextBtn.Location = new System.Drawing.Point(500, 403);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(132, 36);
+            this.nextBtn.TabIndex = 14;
+            this.nextBtn.Text = "Next";
+            this.nextBtn.UseVisualStyleBackColor = true;
+            // 
+            // sangRadio
+            // 
+            this.sangRadio.AutoSize = true;
+            this.sangRadio.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sangRadio.Location = new System.Drawing.Point(236, 255);
+            this.sangRadio.Name = "sangRadio";
+            this.sangRadio.Size = new System.Drawing.Size(90, 33);
+            this.sangRadio.TabIndex = 15;
+            this.sangRadio.TabStop = true;
+            this.sangRadio.Text = "Sáng";
+            this.sangRadio.UseVisualStyleBackColor = true;
+            // 
+            // chieuRadio
+            // 
+            this.chieuRadio.AutoSize = true;
+            this.chieuRadio.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chieuRadio.Location = new System.Drawing.Point(332, 253);
+            this.chieuRadio.Name = "chieuRadio";
+            this.chieuRadio.Size = new System.Drawing.Size(97, 33);
+            this.chieuRadio.TabIndex = 16;
+            this.chieuRadio.TabStop = true;
+            this.chieuRadio.Text = "Chiều";
+            this.chieuRadio.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 450);
+            this.Controls.Add(this.chieuRadio);
+            this.Controls.Add(this.sangRadio);
+            this.Controls.Add(this.nextBtn);
+            this.Controls.Add(this.nhapdanhsachBtn);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tongsophongField);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.monthiField);
+            this.Controls.Add(this.khoangayField);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.kythiField);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tongsophongField)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox kythiField;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker khoangayField;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox monthiField;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown tongsophongField;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button nhapdanhsachBtn;
+        private System.Windows.Forms.Button nextBtn;
+        private System.Windows.Forms.RadioButton sangRadio;
+        private System.Windows.Forms.RadioButton chieuRadio;
+    }
+}
+
---
+++ FaceRecognitionApp/Form1.cs
@@ -0,0 +1,27 @@
+﻿using FaceRecognitionDotNet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FaceRecognitionApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private void nhapdanhsachBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
